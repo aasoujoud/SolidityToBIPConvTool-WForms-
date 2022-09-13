b422c7535a0bc9a70e8a7cfe44fe2a255ff01437
aasoujoud <42418822+aasoujoud@users.noreply.github.com>
2022-09-13 16:19:59 -0400

    Console Version
    Console version of the project

---
+++ SolidityBIPConv/SolidityBIPConvSupport.cs
@@ -0,0 +1,23 @@
+﻿
+
+public static class SolidityBIPSupport
+{
+    private string ReturnStringBtwSemiColons(string originalString)
+    {
+        string FinalString = "";
+
+        for (int i = 0; i < originalString.Length; i++)
+        {
+            if (originalString[i] == '{')  // when current char is '{', start from current length 'i' and get all char storing it in one string
+            {
+                while (i < originalString.Length)
+                {
+                    FinalString += originalString[i];
+                    i++;
+                }
+            }
+        }
+        return FinalString;
+    }
+}
+
